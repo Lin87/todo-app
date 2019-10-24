a7edcdb0112a0cd88fbf13dd1c19a91e3c57ef2b
Ethan Lin <ethan.lin@uwex.edu>
2019-10-24 13:26:10 -0500

    added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
