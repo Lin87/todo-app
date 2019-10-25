c54d79e6d729165ad963e92a79301b06a7d8dddd
Ethan Lin <ethan.lin@uwex.edu>
2019-10-25 10:48:38 -0500

    refactored code to move list of todos to each todo-list-item component

--- src/app/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list-item/todo-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../firestore/task.model';
 
 @Component({
   selector: 'app-todo-list-item',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListItemComponent implements OnInit {
 
+  @Input() todo: Task;
+  
   constructor() { }
 
   ngOnInit() {
