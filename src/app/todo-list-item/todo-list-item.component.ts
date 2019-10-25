dadb791aa1d2fee249a6d24a154b3d900b11ae79
Ethan Lin <ethan.lin@uwex.edu>
2019-10-25 16:13:21 -0500

    add methods to update and remove todo items

--- src/app/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list-item/todo-list-item.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../firestore/task.model';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faSquare, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { TaskService } from '../firestore/task.service';
 
 @Component({
   selector: 'app-todo-list-item',
@@ -12,10 +16,24 @@ export class TodoListItemComponent implements OnInit {
 
   formattedTimestamp: string;
   
-  constructor() { }
+  constructor( private faLibrary: FaIconLibrary, private taskServcice: TaskService ) {
+    faLibrary.addIcons(faTrash, faSquare, faCheckSquare);
+  }
 
   ngOnInit() {
     this.formattedTimestamp = this.todo.timestamp.toDate().toLocaleDateString();
+  }
+
+  changeDueDate(priority: string):void {
+    this.taskServcice.updateTask(this.todo.id, {dueDate: priority});
+  }
+
+  deleteTodo():void {
+    this.taskServcice.deleteTask(this.todo.id);
+  }
+
+  setCompleted():void {
+    this.taskServcice.updateTask(this.todo.id, {completed: !this.todo.completed});
   }
 
 }
